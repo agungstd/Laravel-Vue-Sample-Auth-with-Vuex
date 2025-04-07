d74d4a723b8f771a5f3be5386edc5fe0e1ba87e3
agungstd <apakatadunia11@gmail.com>
2025-04-07 13:25:22 +0700

    Adding fallback to prevent 404 errors in SPA applications with Vue Router

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,18 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{csrf_token()}}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Vue-Router</title>  
+    <title>Vue-Router</title>
   </head>
   <body>
     <div id="app"></div>
+    <script>
+      // Fallback to prevent 404 errors when reloading on nested routes
+      if (!window.history.state && !location.hash) {
+        window.history.replaceState({}, '', '/');
+      }
+    </script>
     <script src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
